e1e09efb520349d217c2a4fa3cfb41faa71c9730
MT-SUPER-POWER <2065838115@qq.com>
2026-03-13 22:24:54 +0800

    feat(types): 完善评论与歌曲类型定义

--- types/api/music.ts
+++ types/api/music.ts
@@ -10,3 +10,34 @@ export interface SongDetail {
   }; // 专辑
   publishTime: number;
 }
+
+
+interface NeteaseUser {
+  userId: number;
+  nickname: string;
+  avatarUrl: string;
+}
+
+interface NeteaseReply {
+  user: NeteaseUser;
+  content: string;
+}
+
+export interface NeteaseComment {
+  commentId: number;
+  user: NeteaseUser;
+  content: string;
+  timeStr: string;
+  likedCount: number;
+  liked: boolean;
+  beReplied: NeteaseReply[];
+}
+
+export interface SongComment {
+  code: number;
+  total: number;
+  more: boolean;
+  moreHot: boolean;
+  hotComments?: NeteaseComment[];
+  comments: NeteaseComment[];
+}
