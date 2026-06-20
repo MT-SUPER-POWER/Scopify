66cb9f9cf807e1ad381c0c0ac57e025f7fdfc72e
MT-SUPER-POWER <MT-SUPER-POWER@users.noreply.github.com>
2026-06-21 06:23:03 +0800

    feat(ui): add View Artist to popular track context menu

--- components/artist/PopularTrackItem.tsx
+++ components/artist/PopularTrackItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Heart, Link2, ListPlus, Pause, Play, PlusCircle } from "lucide-react";
+import { Heart, Link2, ListPlus, Pause, Play, PlusCircle, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { memo, useCallback, useMemo, useState } from "react";
@@ -316,6 +316,37 @@ export const PopularTrackItem = memo(
                 {t("contextMenu.copyLink")}
               </button>
             </ContextMenuItem>
+
+            {/* View Artist */}
+            {track.ar.length > 0 && (
+              track.ar.length === 1 ? (
+                <ContextMenuItem asChild className="focus:bg-white/10 focus:text-white">
+                  <Link href={`/artist?id=${track.ar[0].id}`} className="w-full h-full block">
+                    <User className="w-4 h-4 mr-2" />
+                    {t("contextMenu.goToArtist")}
+                  </Link>
+                </ContextMenuItem>
+              ) : (
+                <ContextMenuSub>
+                  <ContextMenuSubTrigger className="focus:bg-white/10 focus:text-white">
+                    <User className="w-4 h-4 mr-4" />
+                    {t("contextMenu.goToArtist")}
+                  </ContextMenuSubTrigger>
+                  <ContextMenuSubContent className="bg-[#282828] text-white border-white/10">
+                    {track.ar.map((artist) => (
+                      <ContextMenuItem key={artist.id} asChild className="focus:bg-white/10 focus:text-white">
+                        <Link
+                          href={`/artist?id=${artist.id}`}
+                          className="w-full h-full block"
+                        >
+                          {artist.name}
+                        </Link>
+                      </ContextMenuItem>
+                    ))}
+                  </ContextMenuSubContent>
+                </ContextMenuSub>
+              )
+            )}
           </ContextMenuGroup>
         </ContextMenuContent>
       </ContextMenu>
